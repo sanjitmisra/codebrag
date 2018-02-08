a5dc105826b4c43308452869f93e7f29e6ec814e
Sanjit Misra <misra.sanjit@gmail.com>
2018-02-08 15:03:40 +0530

    added powershell script for pulling latest from git server

---
+++ scripts/dist/package_content/pull-repos.ps1
@@ -0,0 +1,37 @@
+﻿$ErrorActionPreference = "SilentlyContinue"
+
+# Get the location of the script. Every other path is relative to this.
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+Set-Location -Path $scriptPath  
+
+foreach ($repo in Get-Content -Path ".\repos\repolist.txt"){
+    echo "In repo - " + $repo
+   
+    # Costruct fully named path of the directory housing the repo
+    $path = [io.path]::Combine($scriptPath,"repos",$repo)
+
+    # In case it is a new repo, clone it
+    if ( -not (Test-Path $path)){
+        
+        # Construct the URL for the repository
+        $repo_url = "github url here" + $repo + ".git"
+
+        #Set path to ../repos/
+        $pathtillrepodir = [io.path]::Combine($scriptPath,"repos")
+        Set-Location -Path $pathtillrepodir
+
+        # Clone the repo
+        git clone $repo_url
+       
+       }
+
+    # Move inside the repo directory
+    Set-Location -Path $path
+
+    # Pull all committed changes from remote
+    git fetch --all
+
+    # Move back to the root directory
+    Set-Location -Path $scriptPath  
+}
